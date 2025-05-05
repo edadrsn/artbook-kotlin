1398d17462322e34f286b0eb1ac90bbbee12a8f7
Eda Dursun <edadrsn929@gmail.com>
2025-05-05 20:52:10 +0300

    art menu created

--- app/src/main/java/com/example/artbookkotlin/MainActivity.kt
+++ app/src/main/java/com/example/artbookkotlin/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.artbookkotlin
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,5 +20,26 @@ class MainActivity : AppCompatActivity() {
         binding=ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+    }
+
+    //MENÜYÜ MAİNE BAĞLAMAK
+
+    //Bağlama işini yapıcaz
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        //inflater
+        val menuInflater=menuInflater
+        menuInflater.inflate(R.menu.art_menu,menu)
+        return super.onCreateOptionsMenu(menu)
+
+    }
+
+    //Menüye tıklanırsa ne yapıcaz
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if(item.itemId==R.id.add_art_item){
+            val intent= Intent(this@MainActivity,MainActivity2::class.java)
+            startActivity(intent)
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
