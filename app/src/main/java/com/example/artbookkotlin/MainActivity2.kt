ae0ea83444bbeee288556b0436b0f7a6f4368f01
Eda Dursun <edadrsn929@gmail.com>
2025-05-05 21:12:23 +0300

    permission checks were made

--- app/src/main/java/com/example/artbookkotlin/MainActivity2.kt
+++ app/src/main/java/com/example/artbookkotlin/MainActivity2.kt
@@ -1,12 +1,19 @@
 package com.example.artbookkotlin
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.artbookkotlin.databinding.ActivityMain2Binding
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity2 : AppCompatActivity() {
 
@@ -24,6 +31,26 @@ class MainActivity2 : AppCompatActivity() {
     }
 
     fun selectImage(view: View){
+        //Daha önceden izin verilmiş mi kontrol et
+        if(ContextCompat.checkSelfPermission(this,Manifest.permission.READ_EXTERNAL_STORAGE)!=PackageManager.PERMISSION_GRANTED){ // -->izin verilmedi demek
+            //İzin iste ve kullanıcıya izin isteme mantığını  açıkla
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.READ_EXTERNAL_STORAGE)){
+                //-rationale-
+                Snackbar.make(view,"Permission Needed For Gallery",Snackbar.LENGTH_INDEFINITE).setAction("Give Permission",View.OnClickListener {
+                    //request permission-izin iste
+
+                }).show()
+            }
+            else{
+                //request permission-izin iste
+            }
+        }
+        else{ //--> izin verildi demek
+            //Galeriye git
+            val intentToGallery= Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            //intent
+
+        }
 
     }
 }
